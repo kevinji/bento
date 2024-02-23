aa6788d9460e3b894e19d5db6653d85249f8f8c1
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2024-02-22 22:44:29 -0800

    Use cfg_if to restrict build to linux

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
-#[cfg(not(target_os = "linux"))]
-compile_error!("Only linux is supported");
+cfg_if::cfg_if! {
+    if #[cfg(target_os = "linux")] {
+        mod child;
+        mod cli;
+        mod container;
+        mod container_config;
+        mod sockets;
+        mod uid_gid_mapping;
 
-mod child;
-mod cli;
-mod container;
-mod container_config;
-mod sockets;
-mod uid_gid_mapping;
-
-pub use cli::Args;
-pub use container::start;
+        pub use cli::Args;
+        pub use container::start;
+    } else {
+        compile_error!("Only linux is supported");
+    }
+}
